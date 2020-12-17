20143201e13b1da0ef88b1ba6decfdfe803f65f2
leo <61103620+luobaotai@users.noreply.github.com>
2020-12-17 15:29:26 +0800

    Create User.kt

---
+++ DataBinding/User.kt
@@ -0,0 +1,10 @@
+package com.example.jetpackdemo
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import androidx.databinding.ObservableField
+import androidx.databinding.ObservableInt
+
+class User(var firstName: String, var lastName: String) {
+
+}
